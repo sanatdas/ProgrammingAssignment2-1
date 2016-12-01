f0278b6b7eee46d2094c2c002faf59e13f781c56
Sanat Das <sanat301@gmail.com>
2016-12-01 19:09:14 +0900

    changes made by Sanat

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,35 @@
-## function makeCacheMatrix gets the input matrix x,
-## set the value the given matrix, get the value of matrix 'x',
-## set the value of inverse of matrix x, get the value of inverse matrix
+# Function "makeCacheMatrix" gets the input matrix x as input,returns functions:
+# "set" to set/reset value of the matrix,
+# "get" to retrive the value of matrix,
+# "setinverse" to set the value of the matrix inverse,
+# "getinverse" to retrieve the value of the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()){
-    cachedInverse <-NULL
-    set <- function(y){
-        x <<- y
-        cachedInverse <<- NULL
-    }
-    get <- function() x
-    setmatrix <- function(inverse) cachedInverse <<- inverse
-    getmatrix <- function() cachedInverse
-    list(set = set, get = get, setmatrix=setmatrix,getmatrix=getmatrix)
+        cachedInverse <-NULL
+        set <- function(y){
+                x <<- y
+                cachedInverse <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) cachedInverse <<- inverse
+        getinverse <- function() cachedInverse
+        list(set=set, get=get, setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## functionacheSolve gets the inverse of the matrix created with above 
-## function.It checks if the inverse matrix is already existing in the cache.
-## If exists, then it gets the details of the matrix from cache.
-## Otherwise, it creates the inverse matrix of the given matrix 'x'
-## and sets the matrix value in the cache using setmatrix function. 
+# function "cacheSolve" gets the inverse of the matrix created with above 
+# function.If the inverse matrix is already computed, it gets the details of 
+# the matrix from cache.Otherwise, it creates the inverse matrix of the given matrix
+# and sets the matrix value in the cache using setmatrix function. 
 
 cacheSolve <- function(x,...){
-    inverse <- x$getmatrix()
-    if(!is.null(inverse)){
-        message("getting cached data")
-        return(inverse)
-    }
-    inputmatrix <- x$get()
-    inverse <- solve(inputmatrix,...)
-    x$setmatrix(inverse)
-    inverse
+        inverse <- x$getinverse()
+        if(!is.null(inverse)){
+                message("getting cached data")
+                return(inverse)
+        }
+        inputmatrix <- x$get()
+        inverse <- solve(inputmatrix,...)
+        x$setinverse(inverse)
+        inverse
 }
